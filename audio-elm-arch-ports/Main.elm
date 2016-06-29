7a164a544a0ee9a0993fbec370487e264f3fdcb2
Vincent Jousse <vincent@jousse.org>
2016-06-29 20:37:00 +0200

    Add play and pause ports

--- audio-elm-arch-ports/Main.elm
+++ audio-elm-arch-ports/Main.elm
@@ -107,3 +107,19 @@ view model =
 
 
 port setCurrentTime : Float -> Cmd msg
+
+
+port play : () -> Cmd msg
+
+
+port pause : () -> Cmd msg
+
+
+playIt : Cmd msg
+playIt =
+    play ()
+
+
+pauseIt : Cmd msg
+pauseIt =
+    pause ()
